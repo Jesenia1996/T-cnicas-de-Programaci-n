67f318286240cff93cb5afc7d011a09f309245d3
LENOVO <jm.montalvano@uea.edu.ec>
2025-06-15 21:21:24 -0500

    Semana_3

--- Comparación de Programación Tradicional y POO en Python/2. TAREA POO.py
+++ Comparación de Programación Tradicional y POO en Python/2. TAREA POO.py
@@ -1,4 +1,6 @@
 # Programa para calcular el Promedio Semanal del Clima Utilizando la POOMore actions
+from typing import Any
+
 
 # Clase principal que contiene los datos y métodos generales
 class ClimaSemanal:
@@ -9,27 +11,27 @@ class ClimaSemanal:
         # Atributo privado con doble guion bajo (encapsulado). No debe ser accedido directamente desde fuera
         self.__temperaturas = []
 
-    # Método público para ingresar temperaturas
+    #metodo público para ingresar temperaturas
     def ingresar_temperaturas(self):
         for dia in self._dias:
             temp = float(input(f"Ingrese la temperatura del día {dia}: "))
             self.__temperaturas.append(temp)
 
-    # Método protegido que devuelve la lista de temperaturas (forma segura de acceder al atributo privado)
-    def _obtener_temperaturas(self):
+    # Metodo protegido que devuelve la lista de temperaturas (forma segura de acceder al atributo privado)
+    def _obtener_temperaturas(self) -> list[Any]:
         return self.__temperaturas
 
 # Clase hija que hereda de ClimaBase (herencia)
 class ClimaPromedio(ClimaSemanal):
-    # Método para calcular el promedio usando las temperaturas de la clase base
+    # Metodo para calcular el promedio usando las temperaturas de la clase base
     def calcular_promedio(self):
-        datos = self._obtener_temperaturas()  # Acceso al atributo privado a través de un método protegido
+        datos = self._obtener_temperaturas()  # Acceso al atributo privado a través de un metodo protegido
         if len(datos) == 0:
             return 0
         promedio = sum(datos) / len(datos)
         return round(promedio, 2)
 
-    # Método para mostrar el resultado final
+    # Metodo para mostrar el resultado final
     def mostrar_resultado(self):
         promedio = self.calcular_promedio()
         print(f"\nEl promedio semanal de temperatura es: {promedio}°C")
@@ -39,5 +41,5 @@ if __name__ == "__main__":
     # Creamos una instancia de la clase hija (heredada)
     clima = ClimaPromedio()
     print("== PROMEDIO SEMANAL DEL CLIMA (POO) ==")
-    clima.ingresar_temperaturas()   # Método heredado de ClimaBase
-    clima.mostrar_resultado()       # Método propio de ClimaPromedio
+    clima.ingresar_temperaturas()   # Metodo heredado de ClimaBase
+    clima.mostrar_resultado()       # Metodo propio de ClimaPromedio
